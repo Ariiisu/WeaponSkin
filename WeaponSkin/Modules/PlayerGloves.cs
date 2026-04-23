d4ed72e69a85fac43e1b94505307753072ba5aec
Nuko <me@nuko.me>
2026-04-23 20:15:38 +0800

    fix ag2 gloves

--- WeaponSkin/Modules/PlayerGloves.cs
+++ WeaponSkin/Modules/PlayerGloves.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging;
 using Sharp.Shared.Enums;
 using Sharp.Shared.HookParams;
 using WeaponSkin.Managers;
@@ -11,11 +11,20 @@ internal class PlayerGloves : IModule
     private readonly IPlayerInfoManager    _playerInfo;
     private readonly ILogger<PlayerGloves> _logger;
 
+    private readonly unsafe delegate* unmanaged<nint, byte, void> CCSPlayerPawn_SetGlovesBodyGroup;
+
     public PlayerGloves(InterfaceBridge bridge, IPlayerInfoManager playerInfo, ILogger<PlayerGloves> logger)
     {
         _bridge     = bridge;
         _playerInfo = playerInfo;
         _logger     = logger;
+
+        unsafe
+        {
+            CCSPlayerPawn_SetGlovesBodyGroup = (delegate* unmanaged<IntPtr, byte, void>) GetSetGlovesBodyGroupAddress();
+
+            _logger.LogInformation("CCSPlayerPawn_SetGlovesBodyGroup 0x{addr:X}", (nint) CCSPlayerPawn_SetGlovesBodyGroup);
+        }
     }
 
     public bool Init()
@@ -48,5 +57,54 @@ internal class PlayerGloves : IModule
         }
 
         pawn.GiveGloves(gloves, cosmetics.PaintId, cosmetics.Wear, (int) cosmetics.Seed);
+
+        unsafe
+        {
+            if (CCSPlayerPawn_SetGlovesBodyGroup is null)
+            {
+                _logger.LogWarning("CCSPlayerPawn_SetGlovesBodyGroup is null, gloves changer may not work properly");
+
+                return;
+            }
+
+            CCSPlayerPawn_SetGlovesBodyGroup(pawn.GetAbsPtr(), 0);
+        }
+    }
+
+    private nint GetSetGlovesBodyGroupAddress()
+    {
+        var server  = _bridge.ModuleManager.Server;
+
+        var first_or_third_personToken = _bridge.ModSharp.MakeStringToken("first_or_third_person");
+
+        var tokenBytes   = BitConverter.GetBytes(first_or_third_personToken);
+        var tokenAddress = server.FindData(tokenBytes, false);
+
+        if (tokenAddress == nint.Zero)
+        {
+            _logger.LogWarning("Cannot find address with first_or_third_person_token(0x{token:X})", first_or_third_personToken);
+
+            return nint.Zero;
+        }
+
+        var           tokenRefs = server.GetReferencesFromPointer(tokenAddress);
+        HashSet<nint> sets      = [];
+
+        foreach (var refs in tokenRefs)
+        {
+            if (server.GetFunctionRange(refs, out var start, out _))
+            {
+                sets.Add(start);
+            }
+        }
+
+        if (sets.Count != 1)
+        {
+            _logger.LogWarning("Expected one function that references first_or_third_person_token but got {size}", sets.Count);
+
+            return nint.Zero;
+        }
+
+        return sets.First();
     }
 }
